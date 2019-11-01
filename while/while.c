8fe25668ddb1e712609ffc004a9c1b1756d7ad2d
EC2 Default User <ec2-user@ip-172-30-1-190.ap-northeast-1.compute.internal>
2019-11-01 12:07:24 +0000

    add while/while.c

---
+++ while/while.c
@@ -0,0 +1,16 @@
+/**/
+/**/
+/*#include<stdio.h>*/
+int printf(const char * /*restrict*/ format, ...);
+int main(int c,char **v){
+    int i=-10;
+    while( i++){
+        printf("パイソン使いだけにな！ %d\n",
+            -i 
+	);
+    };
+    return 0;
+}
+/**/
+/**/
+
